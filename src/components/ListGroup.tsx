3790fb39e1743d3334d79c9364274a87cb356a47
Jani Kaikkonen <jh.kaikkonen@gmail.com>
2023-10-24 12:02:42 +0300

    Cities

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,23 +1,34 @@
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+import { MouseEvent, useState } from "react";
 
-  items = [];
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+}
 
-  const message = items.length === 0 ? <p>No Item Found</p> : null;
+function ListGroup({ items, heading }: ListGroupProps) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
-  return (
-    <>
-      <h1>List</h1>
-      {message}
-    </>
-  );
+  //event handler
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
