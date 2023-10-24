66df3333683eccde89ffaf07e5389ab572f97d33
Jani Kaikkonen <jh.kaikkonen@gmail.com>
2023-10-24 12:41:26 +0300

    Alert First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      Hello <span>World</span>
     </div>
   );
 }
